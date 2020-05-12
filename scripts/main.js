c5552d639358d2148da0500ec4b556794c34ecdc
teobot <theoclapperton@outlook.com>
2020-05-12 05:15:57 +0100

    Added Filters

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var main = new Vue({
         fishData: fishData
     },
     created() {
-        this.fishData = this.fishData.sort((a, b) => (a.fish_name > b.fish_name) ? 1 : -1);
+        this.fishData = this.fishData.sort(this.dynamicsort("fish_name", "asc"))
     },
     methods: {
         priceWithCommas: function(x) {
@@ -12,11 +12,30 @@ var main = new Vue({
         },
         filter_by_price: function() {
             // Filter By Price
-            console.log("filter by price");
-            main.fishData = main.fishData.sort((a, b) => (a.fish_price > b.fish_price) ? 1 : -1);
+            main.fishData.sort(main.dynamicsort("fish_price", "asc"))
+
         },
         filter_by_name: function() {
-            main.fishData = main.fishData.sort((a, b) => (a.fish_name > b.fish_name) ? 1 : -1);
+            // Filter By Name
+            main.fishData.sort(main.dynamicsort("fish_name", "asc"))
         },
+        dynamicsort: function(property, order) {
+            var sort_order = 1;
+            if (order === "desc") {
+                sort_order = -1;
+            }
+            return function(a, b) {
+                // a should come before b in the sorted order
+                if (a[property] < b[property]) {
+                    return -1 * sort_order;
+                    // a should come after b in the sorted order
+                } else if (a[property] > b[property]) {
+                    return 1 * sort_order;
+                    // a and b are the same
+                } else {
+                    return 0 * sort_order;
+                }
+            }
+        }
     }
 })
